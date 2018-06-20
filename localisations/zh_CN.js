db2d4b32a9d683f9d3138f3358c676a8047b8db9
MattyChance <chenmatty.wang@gmail.com>
2018-06-20 12:49:32 -0400

    CHINA(842):added zn_CN

---
+++ localisations/zh_CN.js
@@ -0,0 +1,20 @@
+(function() {
+    $.html5ValidationEngine.localisations.ja = {
+        required: "此项为必填项。",
+        remote: "请修复此项。",
+        email: "请输入一个有效的邮箱地址。",
+        url: "请输入有效的网址。",
+        date: "请输入有效的日期。",
+        number: "请输入有效的号码。",
+        digits: "此项仅支持数字格式。",
+        match : "请保持密码一致。",
+        creditcard: "请输入有效的信用卡卡号。",
+        equalTo: "请输入相同的内容。",
+        maxlength: $.html5ValidationEngine.format("请输入最长{0}个字符。"),
+        minlength: $.html5ValidationEngine.format("请输入至少{0}个字符。"),
+        minmax: $.html5ValidationEngine.format("请输入{0}至{1}个字符。"),
+        range: $.html5ValidationEngine.format("请输入{0}到{1}之间的一个数值。"),
+        max: $.html5ValidationEngine.format("请输入小于或等于{0}的一个数值。"),
+        min: $.html5ValidationEngine.format("请输入大于或等于{0}的一个数值。")
+    };
+}());
